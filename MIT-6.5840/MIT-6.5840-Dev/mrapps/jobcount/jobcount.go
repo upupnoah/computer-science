4b2bd558a4f79fc2d3283772ce329533b07fd8f0
upupnoah <upupqi.cs@gmail.com>
2024-01-10 00:26:16 +0800

    feat(dev): refactor dict structure for MIT6.5840

---
+++ MIT-6.5840/MIT-6.5840-Dev/mrapps/jobcount/jobcount.go
@@ -0,0 +1,48 @@
+package main
+
+//
+// a MapReduce pseudo-application that counts the number of times map/reduce
+// tasks are run, to test whether jobs are assigned multiple times even when
+// there is no failure.
+//
+// go build -buildmode=plugin crash.go
+//
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	"6.5840/mr"
+)
+
+var count int
+
+func Map(filename string, contents string) []mr.KeyValue {
+	me := os.Getpid()
+	f := fmt.Sprintf("mr-worker-jobcount-%d-%d", me, count)
+	count++
+	err := os.WriteFile(f, []byte("x"), 0666)
+	if err != nil {
+		panic(err)
+	}
+	time.Sleep(time.Duration(2000+rand.Intn(3000)) * time.Millisecond)
+	return []mr.KeyValue{mr.KeyValue{Key: "a", Value: "x"}}
+}
+
+func Reduce(key string, values []string) string {
+	files, err := os.ReadDir(".")
+	if err != nil {
+		panic(err)
+	}
+	invocations := 0
+	for _, f := range files {
+		if strings.HasPrefix(f.Name(), "mr-worker-jobcount") {
+			invocations++
+		}
+	}
+	return strconv.Itoa(invocations)
+}
